645bf8bd30e4c16816e29047411c830d35eb8a3a
Sean Sarokas <sps4462@gmail.com>
2023-02-27 15:25:51 -0500

    first commit

--- phase-3-frontend/src/App.js
+++ phase-3-frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Phase 3 Frontend
         </a>
       </header>
     </div>
